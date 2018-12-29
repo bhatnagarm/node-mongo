18d5e3bc725ea5abb61fc10923026d8edc0dafd6
bhatnagarm <mitul.bhatnagar2@virginaustralia.com>
2018-12-29 10:38:17 +1000

    Callback hell and Promises

--- index.js
+++ index.js
@@ -5,33 +5,31 @@ const dboper = require('./operations');
 const url = 'mongodb://localhost:27017';
 const dbname = 'conFusion';
 
-MongoClient.connect(url, (err,client) =>{
-  assert.equal(err,null);
+MongoClient.connect(url).then((client) =>{
 
   console.log('Connected correctly to server');
   const db = client.db(dbname);
 
   dboper.insertDocument(db, {name:'Example',Description:'Test'},
-   "dishes", (result) => {
+   "dishes")
+   .then((result) => {
     console.log('Insert Document: \n',result.ops);
 
-    dboper.findDocument(db, "dishes", (docs) => {
+     return dboper.findDocument(db, "dishes")
+  }).then((docs) => {
       console.log('Found Documents: \n', docs);
-
-      dboper.updateDocument(db, {name:'Example'},
-        {Description:'Updated Test'}, "dishes",
-        (result) => {
+      return dboper.updateDocument(db, {name:'Example'},
+        {Description:'Updated Test'}, "dishes")
+  }).then((result) => {
           console.log("Updated Document\n",result.result);
-
-        dboper.findDocument(db, "dishes", (docs) => {
+          return dboper.findDocument(db, "dishes")
+  }).then((docs) => {
           console.log('Found Documents: \n', docs);
-          db.dropCollection("dishes", (result)=> {
-            console.log('Dropped Collection:', result);
+          return db.dropCollection("dishes")
+  }).then((result)=> {
+          console.log('Dropped Collection:', result);
+          client.close();
+  }).catch((err) => console.log(err));
 
-            client.close();
-          });
-        });
-      });
-    });
-  });
-});
+
+}).catch((err) => console.log(err));
